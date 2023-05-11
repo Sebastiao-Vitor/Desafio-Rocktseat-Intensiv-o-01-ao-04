b39b6f6c53847bdc7bf31ae4659282b7469a0653
Sebastiao Vitor <sebastiaovitor18@gmail.com>
2023-05-11 19:13:54 -0300

    termino

---
+++ main.js
@@ -0,0 +1,99 @@
+
+// Você pode apresentar o resultado tanto no **console** quanto em um **alerta** no navegador.
+
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+// let message = "Hello World!"
+// console.log(message)
+// alert(message)
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+// let numberOne = Number(22)
+// let numberTwo = Number(38)
+// let result = numberOne + numberTwo
+// console.log(`A soma dos dois números é: ${result}`)
+// alert(result)
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+//     💡 Para saber o tipo de dado você pode usar o operador `typeof`
+    
+// let Numbers = 6585
+// let messageOne = "É um número"
+// let messageTwo = "Não é um número"
+// let trueNumber = typeof Numbers == typeof Number()
+// console.log(trueNumber)
+
+// if(trueNumber == true) {
+//   console.log(messageOne)
+// } else {
+//   console.log(trueNumber, messageTwo)
+// }
+
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+// let Stringers =  "Sou uma String kk"
+// let message1 = "É uma string."
+// let message2 = "Não é uma string"
+// let trueString = typeof Stringers == typeof String()
+// console.log(trueString)
+// if(trueString == true) {
+//   console.log(message1)
+// } else {
+//   console.log(message2)
+// }
+
+
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+// let boolean = "sdfajsl8852"
+// let message1 = "É um booleano"
+// let message2 = "Não é um boleano"
+// let trueBoolean = typeof boolean == typeof Boolean()
+// console.log(trueBoolean)
+// if(trueBoolean == true) {
+//   console.log(message1)
+// } else {
+//   console.log(message2)
+// }
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+// let varOne = Number(80)
+// let varTwo = Number(23)
+// let result = varOne - varTwo
+// console.log(`O resultado da Subtração é: ${result}`)
+
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+// let varOne = Number(25)
+// let varTwo = Number(12)
+// let result = varOne * varTwo
+// console.log(`A multiplicação dos dois números é: ${result}`)
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+// let varOne = Number(872)
+// let varTwo = Number(37)
+// let result = varOne / varTwo
+// console.log(`A divisão dos dois números é: ${result.toFixed(1)}`)
+
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+// let numberPar = Number(4813256498017)
+// let truePar = numberPar % 2
+// if(truePar == 0) {
+//   console.log(`É um número par: ${numberPar}`)
+// } else {
+//   console.log(`Não é um número par: ${numberPar}`)
+// }
+
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+// let numberImpar = Number(1345637983)
+// let trueImpar = numberImpar % 2
+// if(trueImpar > 0) {
+//   console.log(`É um número ímpar: ${numberImpar}`)
+// } else {
+//   console.log(`Não é um número ímpar: ${numberImpar}`)
+// }
